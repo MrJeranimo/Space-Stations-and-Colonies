13f8e9d45945cc4d2c26117cf4e0d13f764de023
Jeremy Harrington <harringtonj6@xavier.edu>
2026-02-10 23:03:56 -0500

    Vehicle is now cloned mostly. Tanks refill only to NepetalactoneActinidine_2 Orbit is now a correct clone. Vehicle's main engine is now Controllable.

--- Space Stations and Colonies/SSaCMod.cs
+++ Space Stations and Colonies/SSaCMod.cs
@@ -23,11 +23,40 @@ namespace Space_Stations_and_Colonies
                     // Creates a true clone of the Vehicle's Part Tree
                     PartTree newPartTree = currentV.Parts.Clone();
 
-                    // Refills the tanks to full. Does NOT preserve old tank state
-                    newPartTree.RefillConsumables();
+                    // Stores the original Center of Mass and Bounding Sphere Radius before the new Part Tree is recomputed, as these values will be used to adjust the new Vehicle's position and scale after cloning
+                    double3 centerOfMassAsmb = currentV.CenterOfMassAsmb;
+                    float boundingSphereRadius = currentV.BoundingSphereRadius;
+                    currentV.UpdateAfterPartTreeModification();
 
                     // There is still an error copying this Vehicle, figure out why
                     Vehicle newVehicle = Vehicle.CreateVehicle(currentV.System, currentV.Body2Cce, currentV.BodyRates, currentV.Parent, $"{currentV.Id}_Clone{i}", newPartTree.Root, currentV.Orbit);
+
+                    // Refills tanks, but only to NepetalactoneActinidine_2 :(
+                    newVehicle.RefillConsumables();
+
+                    // Recomputes all derived data for the new Part Tree, such as mass, rocket controls, etc.
+                    newPartTree.RecomputeAllDerivedData();
+
+                    StateVectors stateVectors = currentV.Orbit.StateVectors;
+
+                    double3 double3_2 = (newVehicle.CenterOfMassAsmb - centerOfMassAsmb).Transform(currentV.GetAsmb2Cci());
+
+                    Orbit fromStateCci1 = Orbit.CreateFromStateCci(currentV.Parent, stateVectors.StateTime, stateVectors.PositionCci + double3_2, stateVectors.VelocityCci, currentV.OrbitColor);
+
+                    newVehicle.Teleport(fromStateCci1, new doubleQuat?(), new double3?());
+
+                    double3 double3_3 = (currentV.CenterOfMassAsmb - centerOfMassAsmb).Transform(currentV.GetAsmb2Cci());
+
+                    Orbit fromStateCci2 = Orbit.CreateFromStateCci(currentV.Parent, stateVectors.StateTime, stateVectors.PositionCci + double3_3, stateVectors.VelocityCci, currentV.OrbitColor);
+
+                    currentV.Teleport(fromStateCci2, new doubleQuat?(), new double3?());
+
+                    currentV.OrbitView.DistancePower *= (double)boundingSphereRadius / (double)currentV.BoundingSphereRadius;
+
+                    Program.MainViewport.OrbitController.DistancePower = currentV.OrbitView.DistancePower;
+
+                    currentV.Parent.Children.Add((Astronomical)newVehicle);
+
                     newVehicle.UpdatePerFrameData();
                     i++;
                     Universe.CurrentSystem!.Vehicles.Register(newVehicle);
